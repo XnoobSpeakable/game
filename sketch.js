183434e2b0a42b6efe89e7e2c329f1ccc662f175
XnoobSpeakable <62246474+XnoobSpeakable@users.noreply.github.com>
2021-04-02 13:01:53 +0300

    scaling auto increment upgrade cost

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var x = 0;
 var addnumY = 1;
 var boughtautoinc = 0;
 var autoincspeed = 2000
+var autoinccost = 50
 function setup() {
   createCanvas(600, 600);
    button = createButton('increment');
@@ -25,7 +26,8 @@ function draw() {
   textSize(32);
 text(x, 150, 40);
   textSize(14);
-  text('makes the increment add\none more number per click\nCost: 50', 50, 190);
+  text('makes the increment add\none more number per click\nCost:', 50, 190);
+  text(autoinccost, 100, 190)
   textSize(14);
   text('automatically increments\nthe number once every second\nupgrading again makes it\ntwice as fast\nCost: 50', 250, 190);
 }
@@ -39,8 +41,9 @@ function upgrade() {
   }
 }
 function autoinc() {
-  if(x > 49) {
-    x = x - 50;
+  if(x >= autoinccost) {
+    x = x - autoinccost;
+    autoinccost = autoinccost * 2
     boughtautoinc = 1
     autoincspeed = autoincspeed/2
     window.clearInterval()
